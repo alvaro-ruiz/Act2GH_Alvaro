bb5fe9ef6c2be85da309efc9b5b8fb66089a93ef
alvaro-ruiz <aaruizblanquez@gmail.com>
2023-05-26 19:48:14 +0200

    Añado metodo imprimir

--- GeneradorNumeros.java
+++ GeneradorNumeros.java
@@ -1,6 +1,5 @@
 
 public class GeneradorNumeros {
-
   
   public static void main(String[] args) {
     
@@ -8,6 +7,12 @@ public class GeneradorNumeros {
     
     for (int i = 0; i < 20; i++) {
       numeros[i] = (int)(Math.random() * (10 - 1 + 1) + 1);
+    }
+  }
+  
+  private void imprimir (int[] numeros) {
+    for (int i = 0; i < numeros.length; i++) {
+      System.out.print(numeros[i] + ", ");
     }
   }
 
