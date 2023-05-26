1b42a27eea633bbff595a5a7d8af382a2705775d
alvaro-ruiz <aaruizblanquez@gmail.com>
2023-05-26 20:07:24 +0200

    GeneraNumeros basico pero en vez de 10 numeros son 20

--- GeneradorNumeros.java
+++ GeneradorNumeros.java
@@ -3,17 +3,11 @@ public class GeneradorNumeros {
   
   public static void main(String[] args) {
     
-    int[] numeros = new int[10];
+    int[] numeros = new int[20];
     
     for (int i = 0; i < 20; i++) {
       numeros[i] = (int)(Math.random() * (10 - 1 + 1) + 1);
     }
   }
   
-  private void imprimir (int[] numeros) {
-    for (int i = 0; i < numeros.length; i++) {
-      System.out.print(numeros[i] + ", ");
-    }
-  }
-
 }
