0411a8f2dd8702061e78a5dcb7fc4909c0a9e232
alvaro-ruiz <aaruizblanquez@gmail.com>
2023-05-26 19:33:53 +0200

    Añado Genera Numero Aleatorio

---
+++ GeneradorNumeros.java
@@ -0,0 +1,14 @@
+
+public class GeneradorNumeros {
+
+  
+  public static void main(String[] args) {
+    
+    int[] numeros = new int[10];
+    
+    for (int i = 0; i < 20; i++) {
+      numeros[i] = (int)(Math.random() * (10 - 1 + 1) + 1);
+    }
+  }
+
+}
